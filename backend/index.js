68186d0343fb4fa874b37f1440c3861c58767d0e
Marcus Vinicius Galdino Medeiros <tinuthas@Marcuss-MacBook-Pro.local>
2020-01-14 23:35:22 -0300

    add mongo db

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,12 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
 
+mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-hjkkk.mongodb.net/week10?retryWrites=true&w=majority', {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+})
 app.use(express.json())
 // GET, POST, PUT, DELETE
 
@@ -9,6 +14,8 @@ app.use(express.json())
 // QUERY PARAMS: request.query
 // ROUTE PARAMS: request.params
 // BODY
+
+// MONGO DB -> Nao relacional
 
 app.post('/users', (request, response) => {
     console.log(request.body)
