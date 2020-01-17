396cc9fbed8acb0bc63d2b8e41c11e6070499b02
Marcus Vinicius Galdino Medeiros <tinuthas@Marcuss-MacBook-Pro.local>
2020-01-17 09:51:58 -0300

    index method get

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,6 +5,8 @@ const DevController = require('./controllers/DevController')
 
 const router = Router();
 
+router.get('/devs', DevController.index)
+
 router.post('/devs', DevController.store)
 
 module.exports = router
