20093336dcf4d5f0d5fd0a6fbb9bb0f1f1f5b461
Marcus Vinicius Galdino Medeiros <tinuthas@Marcuss-MacBook-Pro.local>
2020-01-17 09:30:31 -0300

    location post create user

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -7,7 +7,7 @@ const Dev = require('./models/Dev')
 const router = Router();
 
 router.post('/devs', async (request, response) => {
-    const { github_username, techs} = request.body
+    const { github_username, techs, latitude, longitude} = request.body
 
     const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`)
     const  {name = login, avatar_url, bio} = apiResponse.data
@@ -17,13 +17,22 @@ router.post('/devs', async (request, response) => {
     const techsArray = techs.split(',').map(tech => tech.trim())
 
     
+  
+
+    const location = {
+        type: 'Point',
+        coordinates: [longitude, latitude]
+    }
+
     const dev = await Dev.create({
         name,
         github_username,
          avatar_url,
          bio,
          techs: techsArray,
+         location
     })
+
     console.log(name, avatar_url, bio, github_username)
 
     return response.json({dev})
