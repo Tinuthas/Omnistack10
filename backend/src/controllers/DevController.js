a416b69d68632d33a81bfe6fbc9d3b25e41ad4a2
Marcus Vinicius Galdino Medeiros <tinuthas@Marcuss-MacBook-Pro.local>
2020-01-17 09:34:11 -0300

    controller created

---
+++ backend/src/controllers/DevController.js
@@ -0,0 +1,31 @@
+const axios = require('axios')
+const Dev = require('../models/Dev')
+
+module.exports = {
+    async store(request, response)  {
+        const { github_username, techs, latitude, longitude} = request.body 
+
+
+        const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`)
+        const  {name = login, avatar_url, bio} = apiResponse.data
+        const techsArray = techs.split(',').map(tech => tech.trim())
+    
+        const location = {
+            type: 'Point',
+            coordinates: [longitude, latitude]
+        }
+    
+        const dev = await Dev.create({
+            name,
+            github_username,
+             avatar_url,
+             bio,
+             techs: techsArray,
+             location
+        })
+    
+        console.log(name, avatar_url, bio, github_username)
+    
+        return response.json({dev})
+    }
+}
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,42 +1,11 @@
 
 
 const { Router } = require('express')
-const axios = require('axios')
-const Dev = require('./models/Dev')
+const DevController = require('./controllers/DevController')
 
 const router = Router();
 
-router.post('/devs', async (request, response) => {
-    const { github_username, techs, latitude, longitude} = request.body
-
-    const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`)
-    const  {name = login, avatar_url, bio} = apiResponse.data
-
-
-    console.log(name, avatar_url, bio, github_username)
-    const techsArray = techs.split(',').map(tech => tech.trim())
-
-    
-  
-
-    const location = {
-        type: 'Point',
-        coordinates: [longitude, latitude]
-    }
-
-    const dev = await Dev.create({
-        name,
-        github_username,
-         avatar_url,
-         bio,
-         techs: techsArray,
-         location
-    })
-
-    console.log(name, avatar_url, bio, github_username)
-
-    return response.json({dev})
-})
+router.post('/devs', DevController.store)
 
 /*router.get('/devs', (req, res) => {
     return res.json({"success": "success"})
